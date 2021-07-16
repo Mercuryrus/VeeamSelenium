3c7b62929edec63f953a605028b4ff6b70725f1a
Илья Аникин <mercury2612198@gmail.com>
2021-07-16 22:03:30 +0300

    Добавьте файлы проекта.

---
+++ Logic.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace VeeamSelenium
+{
+    class Logic
+    {
+        static public void SelectCheckBox(string text, IWebDriver driver)
+        {
+            driver.FindElement(By.XPath("/html/body/div[1]/div/div[1]/div/div[2]/div[1]/div/div[3]/div/div/button")).Click();
+            driver.FindElement(By.XPath($"//*[@class='custom-control custom-checkbox']//*[text()='{text}']")).Click();
+        }
+        static public void ClickFindElement(string text, IWebDriver driver)
+        {
+            driver.FindElement(By.XPath("/html/body/div[1]/div/div[1]/div/div[2]/div[1]/div/div[2]/div/div")).Click();
+            var vacanciesList = driver.FindElement(By.XPath("/html/body/div[1]/div/div[1]/div/div[2]/div[1]/div/div[2]"));
+            vacanciesList.FindElement(By.PartialLinkText(text)).Click();
+        }
+        static public int GetCountFindElementsCssSelector(IWebDriver driver)
+        {
+            return driver.FindElements(By.XPath("//*[@class='card card-no-hover card-sm']")).Count();
+        }
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace VeeamSelenium
+{
+    public class Tests
+    {
+        public IWebDriver driver;
+        [SetUp]
+        public void Setup()
+        {
+            driver = new OpenQA.Selenium.Chrome.ChromeDriver();
+            driver.Navigate().GoToUrl("https://careers.veeam.ru/vacancies");
+            driver.Manage().Window.Maximize();
+        }
+
+        [Test]
+        public void CountVacancies()
+        {
+            Logic.SelectCheckBox("Английский", driver);
+            Logic.ClickFindElement("Разработка продуктов", driver);
+
+            Assert.AreEqual(6, Logic.GetCountFindElementsCssSelector(driver));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+
+        }
+    }
+}
